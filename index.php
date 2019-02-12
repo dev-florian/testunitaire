6cac7da8adc4ce345cd7d55b2749639ccbb82c3e
florianthe <f.the@sfr.fr>
2019-02-12 14:43:22 +0100

    .

--- index.php
+++ index.php
@@ -26,6 +26,24 @@ Require "src/Calcul.php";
 
         <input type="submit" name="submit2" value="calculer">
     </form>
+
+    <h1>CHOICE</h1>
+    <form name="form3" action="" method="post">
+        <label for="number1">Premier chiffre</label>
+        <input type="number" name="number1" step="1">
+        <br>
+        <select name="choice_calcul">
+            <option value="add">+</option>
+            <option value="multiply">*</option>
+        </select>
+        <br>
+        <label for="number2">Deuxieme chiffre</label>
+        <input type="number" name="number2" step="1">
+
+
+
+        <input type="submit" name="submit3" value="valider">
+    </form>
 <?php
 
 if (isset($_POST['submit'])) {
@@ -37,5 +55,16 @@ if (isset($_POST['submit'])) {
 if (isset($_POST['submit2'])) {
     $init = new Calcul();
     $res = $init->multiply($_POST['number1'], $_POST['number2']);
+    echo "<h3>Le resultat est de " . $res . "</h3>";
+}
+
+if (isset($_POST['submit3'])) {
+    $init = new Calcul();
+    if($_POST["choice_calcul"] == "add"){
+        $res = $init->add($_POST['number1'], $_POST['number2']);
+    }
+    if($_POST["choice_calcul"] == "multiply"){
+        $res = $init->multiply($_POST['number1'], $_POST['number2']);
+    }
     echo "<h3>Le resultat est de " . $res . "</h3>";
 }
